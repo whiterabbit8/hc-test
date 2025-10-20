52138594703655fe2fbe896e04a6c141839ac8d6
Natallia Iv <natashaivanyuk@gmail.com>
2025-10-20 21:24:17 +0300

    feat: add loading question

--- script.js
+++ script.js
@@ -8,7 +8,29 @@ const addQuestionsNumbers = () => {
             'beforeend',
             `<div class="question">В${i}</div>`
         )
-    };
+    }
 }
 
+const loadQuestion = (number = 0) => {
+    const question = questions.questions[number].content;
+    const answers = questions.questions[number].options;
+    const questionElement = document.querySelector('.legend');
+    const answersElement = document.querySelector('.answers');
+
+    questionElement.innerHTML = question;
+    
+    for (let i = 0; i < answers.length; i++) {
+        answersElement.insertAdjacentHTML(
+        'beforeend',
+        `<div class="answer">
+            <input type="radio" id="answer${i}" name="answer" value="answer${i}" />
+            <label for="answer${i}">${answers[i]}</label>
+        </div>`
+        )
+    }    
+}
+
+
+
 addQuestionsNumbers();
+loadQuestion();
